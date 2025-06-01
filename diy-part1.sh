e018e087b726064378329e99d65a5e4a7cb88d6c
toobye <yu3350@live.cn>
2025-06-02 01:10:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-# echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
-# echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+# kenzo
+echo 'src-git kenzo https://github.com/kenzok8/small-package' >>feeds.conf.default
